075d7bf14a03b34a0245a0a5ed61494b20cafc0c
chenxiaowu <five3@163.com>
2023-07-01 19:09:10 +0800

    跨域方法添加

--- util.py
+++ util.py
@@ -5,5 +5,7 @@ def warp_resp(txt):
     rep = make_response(txt)
     rep.status_code = 200
     rep.headers["Access-Control-Allow-Origin"] = "*"
+    rep.headers["Access-Control-Allow-Credentials"] = "true"
+    rep.headers["Access-Control-Allow-Methods"] = "POST, PUT, GET, OPTIONS, DELETE"
 
     return rep
